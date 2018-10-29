7426a1fb97749473b1c21c64563e339688a43824
DaftR <roelandtsruben@hotmail.com>
2018-10-29 21:22:22 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/style.css" />
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+<div id="output">
+<p>Welcome, new messages will be displayed below:</p>
+</div>
+
+<div id="participants">
+<p>Me</p>
+</div>
+
+<div id="input">
+<input type="textarea">
+</div>
+</body>
+</html>
+<head>
+
+</head>
+
